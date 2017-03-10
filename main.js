54f2bbbeaec813674f4564a2fd15f22b2848c7e3
asteriskman7 <asteriskman@gmail.com>
2017-03-09 22:25:45 -0500

    more changes

--- main.js
+++ main.js
@@ -2,9 +2,23 @@
 
 var game = {
   div_msg: undefined,
+  size: 0.75,
   init: function() {
     console.log('init');
     game.div_msg = document.getElementById('div_message');
+    document.getElementById('menu_game').onclick = function() {game.showTab('game');};
+    document.getElementById('menu_stats').onclick = function() {game.showTab('stats');};
+    document.getElementById('menu_about').onclick = function() {game.showTab('about');};
+  },
+  showTab: function(tabName) {
+    ['game', 'stats', 'about'].forEach(function(v) {
+      var e = document.getElementById('div_' + v);
+      if (v === tabName) {
+        e.style.display = 'flex';
+      } else {
+        e.style.display = 'none';
+      }
+    });
   },
   msg: function(text) {
     var waitTime = Math.max(text.length * 3000 / 25, 2000);
